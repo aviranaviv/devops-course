2b5d4e02258cb8ef1efebd53cb461281070de888
Aviran Aviv <103700423+aviranaviv@users.noreply.github.com>
2025-10-31 08:46:23 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+def welcome(username):
+    print(f'Hi {username} and welcome to the World of Games: The Epic Journey\n')
+
+def start_play():
+    games_options = ['Memory Game', 'Guess Game', 'Currency Roulette']
+
+    print('Please choose a game to play:')
+    print('1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back.\n'
+          '2. Guess Game - guess a number and see if you chose like the computer.\n'
+          '3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n')
+
+    while True:
+        user_input = int(input())
+        if user_input > len(games_options) or user_input < 1:
+            print('Try Again you must to choose 1-3')
+        else: break
+
+    print('Please choose a game level between 1-5:')
+    while True:
+        level = int(input())
+        if level < 1 or level > 5:
+            print('Try Again you must to choose 1-5')
+        else: break
+
+    print(f'Ok you chose the game "{games_options[user_input - 1]}" with level {level}')
